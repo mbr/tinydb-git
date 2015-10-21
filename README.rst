189442b89888b449b6906e5e6b42ff84bf173661
Marc Brinkmann <git@marcbrinkmann.de>
2015-10-22 00:53:15 +0200

    Initial import.

---
+++ README.rst
@@ -0,0 +1,4 @@
+tinydb-git
+==========
+
+A storage backend for tinydb that stores database changes inside a git branch.
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='tinydb-git',
+    version='0.1.dev1',
+    description='A git-based storage backend for tinydb.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='https://github.com/mbr/tinydb-git',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+    ]
+)
