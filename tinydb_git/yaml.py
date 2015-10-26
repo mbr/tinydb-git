157d81b96d97544923f5f1383ec89c796bef2b69
Marc Brinkmann <git@marcbrinkmann.de>
2015-10-27 00:39:16 +0100

    Use a custom dumper for nicer yaml output.

--- tinydb_git/yaml.py
+++ tinydb_git/yaml.py
@@ -1,8 +1,19 @@
 from __future__ import absolute_import
 
+from tinydb.database import Element
 import yaml
 
 from . import GitStorage
+
+
+# using a SafeDumper causes no tags to be written, which is much easier on the
+# eyes, however it means we need to add a representer for the TinyDB database
+# element:
+class TinyDBDumper(yaml.SafeDumper):
+    pass
+
+
+TinyDBDumper.add_representer(Element, TinyDBDumper.represent_dict)
 
 
 class YAMLGitStorage(GitStorage):
@@ -13,6 +24,7 @@ class YAMLGitStorage(GitStorage):
 
     def _serialize(self, data):
         return yaml.dump(data,
+                         Dumper=TinyDBDumper,
                          indent=2,
                          default_flow_style=False,
                          encoding=self.ENCODING)
